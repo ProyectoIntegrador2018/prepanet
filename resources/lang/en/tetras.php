38eb26bb9f892371fbbbced311d79dbb7f60f53a
Pedro Ivan Martinez <pedroivanmartinez23@gmail.com>
2018-11-05 17:27:50 -0600

    tetras synaxis

---
+++ resources/lang/en/tetras.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Campus Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'tetras' => 'Tetra',
+    'tetra' => 'Tetra',
+    'new_tetra' => 'Nuevo tetra',
+    'no_tetras' => 'No existen tetra ahorita ',
+    'name' => 'Nombre',
+    'delete_tetra' => 'Borrar tetra',
+    'save' => 'Guardar',
+    'no_tetra' => 'No hay tetra',
+    'try_adding' => '¡Prueba agregando uno!',
+
+    'error_add_tetra' => 'Error creando el tetra. Prueba nuevamente más tarde.',
+    'new_tetra_created' => 'Tetra creado exitosamente!',
+    'error_edit_tetra' => 'Error al editar el tetra. Prueba nuevamente más tarde.',
+    'success_edit_tetra' => '¡Tetra editado satisfactoriamente!',
+    'error_delete_tetra' => 'Error al eliminar el tetra. Prueba nuevamente más tarde',
+    'success_delete_tetra' => '¡Tetra eliminado satisfactoriamente!',
+];
