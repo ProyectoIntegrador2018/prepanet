f87e4a413b88aad91e1805b3aeaad9a2d2f6e73a
Pedro Ivan Martinez <pedroivanmartinez23@gmail.com>
2018-11-05 18:46:29 -0600

    tutors blade fixed syntaxis

--- resources/views/tutors/tutors.blade.php
+++ resources/views/tutors/tutors.blade.php
@@ -4,18 +4,19 @@
 @stop
 
 @section('content')
-    <div id="tutores">
+    <div id="tutor">
         <div class="section">
             <div id="main" class="container">
                 <div class="row grid">
                     @if($tutores->count())
                         @foreach ($tutores as $tutor)
                             <div class="col s12 m6 l4 grid-item">
-                                <a href="{{route('tutor', ["tutor" => tutor->id])}}">
+                                <a href="{{route('tutor', ["tutor" => $tutor->id])}}">
                                     <div class="bordered-card card hoverable border-primary-color">
                                         <div class="card-content">
                                             <div class="row">
-                                                <h5 class="bold black-text">{{$tutor->name}}</h5>
+                                                <h5 class="bold black-text">{{$tutor->user->first_name}} {{$tutor->user->last_name}}</h5>
+                                                <h5 class="black-text">{{$tutor->tetra->name}}</h5>
                                             </div>
                                         </div>
                                     </div>
@@ -24,7 +25,7 @@
                         @endforeach
                     @else
                         <h3 class="center"><i class="material-icons big-icon">sentiment_dissatisfied</i></h3>
-                        <h3 class="center">{{__('tutor.no_tutores')}}</h3>
+                        <h3 class="center">{{__('tutores.no_tutor')}}</h3>
                         <p class="center"><a href="#new-tutor-modal" class="modal-trigger">{{__('tutores.try_adding')}}</a></p>
                     @endif
                 </div>
@@ -34,12 +35,53 @@
             <div class="modal-content">
                 <form class="col s12" role="form" method="POST">
                     @csrf
-
                     <h4 id="modal-title">{{__('tutores.new_tutor')}}</h4>
                     <div class="row">
                         <div class="input-field col s12">
-                            <label for="name">{{__('tutores.name')}}</label>
-                            <input id="name" type="text" name="name" required class="validate">
+                            <label for="email">{{__('tutores.email')}}</label>
+                            <input id="email" type="text" name="email" required class="validate">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <label for="password">{{__('tutores.password')}}</label>
+                            <input id="password" type="password" name="password" required class="validate">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <label for="confirm-password">{{__('tutores.confirm_password')}}</label>
+                            <input id="confirm-password" type="password" name="confirm-password" required class="validate">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <label for="first_name">{{__('tutores.first_name')}}</label>
+                            <input id="first_name" type="text" name="first_name" required class="validate">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <label for="last_name">{{__('tutores.last_name')}}</label>
+                            <input id="last_name" type="text" name="last_name" required class="validate">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <select id="tetra" name="tetra">
+                                <option value="" disabled selected>{{__('tutores.select_tetra')}}</option>
+
+                                @foreach($tetras as $tetra)
+                                <option value="{{$tetra->id}}">{{$tetra->name}}</option>
+                                @endforeach
+
+                            </select>
+                            <label for="tetra">{{__('tutores.tetra')}}</label>
                         </div>
                     </div>
 
